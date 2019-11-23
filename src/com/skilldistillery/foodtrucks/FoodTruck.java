a6b5cf51fa0d9bad088a3971dabe023905fd001c
kaishu83 <kaishu83@gmail.com>
2019-11-23 13:25:16 -0700

    completed fuctions

---
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -0,0 +1,59 @@
+package com.skilldistillery.foodtrucks;
+
+public class FoodTruck {
+// F I E L D
+	private int id;
+	private String name;
+	private int rate;
+
+	private String food;
+
+// C O N S T R U C T O R
+
+	public FoodTruck(int id, String name, int rate, String food) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.rate = rate;
+		this.food = food;
+	}
+
+// M E T H O D
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Truck ID-" + id + "\tname-" + name + "\tfood-" + food + "\trate-" + rate;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+
+}
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -1,10 +1,126 @@
 package com.skilldistillery.foodtrucks;
 
+import java.util.*;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		FoodTruckApp app = new FoodTruckApp();
+
+		FoodTruck[] trucks = new FoodTruck[5];
+
+		Scanner input = new Scanner(System.in);
+		app.Welcome();
+		int i = 0;
+
+		while (i < 6) {
+			trucks[i] = new FoodTruck(i + 1, "name", 0, "food");
+
+			System.out.println(
+					"Please enter the name of your next FoodTruck, enter \"quit\" to if don't have another truck to enter");
+			trucks[i].setName(input.nextLine());
+			if (trucks[i].getName().equalsIgnoreCase("quit")) {
+				trucks[i] = null;
+				break;
+			}
+
+			System.out.println("Please enter the food this truck serves: ");
+			trucks[i].setFood(input.nextLine());
+
+			while (trucks[i].getRate() > 5 || trucks[i].getRate() < 1) {
+				System.out.println("Please enter your rate (1-5) for this truck");
+				trucks[i].setRate(input.nextInt());
+				input.nextLine();
+			}
+
+			System.out.println("FoodTruck " + trucks[i].getName() + " created!\n");
+			i++;
+		}
+
+		app.PrintTrucks(trucks);
+
+		int j = 0;
+		while (j != 4) {
+			app.Menu();
+			j = input.nextInt();
+			switch (j) {
+				case 1:
+
+					app.PrintTrucks(trucks);
+					break;
+				case 2:
+
+					System.out.println("The average rating for these foodtrucks is " + app.AverageRate(trucks));
+					System.out.println();
+					break;
+				case 3:
+					System.out.println("Truck with highest rate is: ");
+					System.out.println(app.HighestRate(trucks).toString());
+					System.out.println();
+					break;
+				case 4:
+
+					app.PrintGoodbye();
+					System.out.println();
+					break;
+				default:
+					System.out.println("Wrong option, please choose again");
+					System.out.println();
+			}
+		}
 	}
 
+	public void Welcome() {
+		System.out.println("Welcome to the best foodtruck review site");
+	}
+
+	public void Menu() {
+		System.out.println("1.List all existing food trucks." + "\n2.See the average rating of food trucks"
+				+ "\n3.Display the highest-rated food truck." + "\n4.Quit the program.\nChoose your option");
+		System.out.println();
+	}
+
+	public void PrintTrucks(FoodTruck trucks[]) {
+
+		int i = 0;
+		while (trucks[i] != null && i < 6) {
+			System.out.println(trucks[i].toString());
+			i++;
+		}
+		System.out.println();
+	}
+
+	public void PrintGoodbye() {
+		System.out.println("Thank you for using this app, goodbye");
+	}
+
+	public FoodTruck HighestRate(FoodTruck trucks[]) {
+
+		int i = 0;
+		int index = 0;
+		int rate = trucks[0].getRate();
+		while (trucks[i] != null && i < 6) {
+			if (trucks[i].getRate() > rate) {
+
+				index = i;
+			}
+			i++;
+
+		}
+		return trucks[index];
+	}
+
+	public double AverageRate(FoodTruck trucks[]) {
+		int i = 0;
+
+		int totalRate = 0;
+		while (trucks[i] != null && i < 6) {
+
+			totalRate += trucks[i].getRate();
+			i++;
+		}
+		double averageRate = (double) (totalRate) / i;
+		return averageRate;
+	}
 }
